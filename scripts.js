b25a199f5c6ed33b4bea6e651b3eb30695de9ebd
Furio Alessio <alefurio@hotmail.it>
2020-10-14 16:51:07 +0200

    verifica mail in un array

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+// Esercitazione 1: Check lista email
+// Creare un array con un insieme di indirizzi email.
+// Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
+// Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
+
+
+
+// dichiarazione array
+var array_mail = ['pippodisney@gmail.com', 'plutodisney@gmail.com', 'topolinodisney@gmail.com', 'paperinodisney@gmail.com', 'minniedisney@gmail.com'];
+console.log('Il tuo array contiene: ' + array_mail.length + ' elementi');
+
+// chiedere indirizzo mail all' utente
+var mail_utente = prompt('Qual\'è il tuo indirizzo mail?');
+console.log('Il tuo indirizzo mail è: ' + mail_utente);
+
+// verifico se mail_utente è presente nell' array_mail
+
+for (var i = 0; i < array_mail.length; i++) {
+    if (mail_utente == array_mail[i]) {
+        console.log('sei in lista');
+    }
+    else {
+        console.log('non sei in lista');
+    }
+
+}
